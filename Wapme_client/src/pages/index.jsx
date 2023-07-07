0a176d74751cd36dff6225af04427b66faffbce1
sherlockholms221B <chineduchristsanctus020@gmail.com>
2023-07-07 02:01:31 +0100

    index.js

--- Wapme_client/src/pages/index.jsx
+++ Wapme_client/src/pages/index.jsx
@@ -1,7 +1,24 @@
-import React from "react";
+import React from 'react';
 
-function index() {
-  return <div>index</div>;
+function Introduction() {
+  const name = 'Dear';
+  const projectName = 'Wapme';
+
+  return (
+    <div className='bg-blue-500 p-8 text-white h-screen w-screen flex justify-center items-center flex-col'>
+      <h1 className='text-4xl font-bold'>Hi {name},</h1>
+      <p className='text-xl mt-4'>
+        I'm thrilled to introduce you to{' '}
+        <span className='underline font-bold tracking-wide text-lg'>
+          {projectName}
+        </span>
+        , a WhatsApp clone project.
+      </p>
+      <p className='mt-8 text-black'>
+        © {new Date().getFullYear()} @sanctus. All rights reserved.
+      </p>
+    </div>
+  );
 }
 
-export default index;
+export default Introduction;
