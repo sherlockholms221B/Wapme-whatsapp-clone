85cf86396a384811581c664cd278c019c292a57c
sherlockholms221B <chineduchristsanctus020@gmail.com>
2023-07-07 09:19:31 +0100

    complete set up

--- Wapme_client/src/pages/index.jsx
+++ Wapme_client/src/pages/index.jsx
@@ -6,17 +6,7 @@ function Introduction() {
 
   return (
     <div className='bg-blue-500 p-8 text-white h-screen w-screen flex justify-center items-center flex-col'>
-      <h1 className='text-4xl font-bold'>Hi {name},</h1>
-      <p className='text-xl mt-4'>
-        I'm thrilled to introduce you to{' '}
-        <span className='underline font-bold tracking-wide text-lg'>
-          {projectName}
-        </span>
-        , a WhatsApp clone project.
-      </p>
-      <p className='mt-8 text-black'>
-        © {new Date().getFullYear()} @sanctus. All rights reserved.
-      </p>
+      setup complete
     </div>
   );
 }
