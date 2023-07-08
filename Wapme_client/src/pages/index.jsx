baeed7200c45bb5d7522b1ce2348609269f2c20e
sherlockholms221B <chineduchristsanctus020@gmail.com>
2023-07-08 16:58:14 +0100

    folder structure

--- Wapme_client/src/pages/index.jsx
+++ Wapme_client/src/pages/index.jsx
@@ -1,13 +1,33 @@
-import React from 'react';
+import * as React from 'react';
+// import { Route, Routes } from '';
 
 function Introduction() {
-  const name = 'Dear';
-  const projectName = 'Wapme';
+  const [value, setValue] = React.useState('');
+  // scroll into veiw on route change
+  React.useEffect(() => {
+    document.documentElement.scrollIntoView(0, 0);
+  }, []);
 
   return (
-    <div className='bg-blue-500 p-8 text-white h-screen w-screen flex justify-center items-center flex-col'>
-      setup complete
-    </div>
+    <section
+      role='container'
+      className='bg-dropdown-background text-white h-screen w-screen'
+    >
+      {/* navigation bar component */}
+
+      <div className='border-4 border-red-500 h-fit w-full shadow-lg z-10 flex justify-start gap-6 items-center'>
+        <div className='rounded-full w-12 h-12 bg-white' id='profile'></div>
+        <div className='w-[50%] h-full'>
+          <input
+            name='search'
+            type='text'
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
+            className='w-full outline-none rounded-md '
+          />
+        </div>
+      </div>
+    </section>
   );
 }
 
